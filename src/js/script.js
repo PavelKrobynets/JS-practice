d15979bc325b58537441a48a521ec8706db45704
Pavel <krobynetspavel@gmail.com>
2024-05-06 11:17:44 +0300

    post-date processing

--- src/js/script.js
+++ src/js/script.js
@@ -45,11 +45,20 @@ window.addEventListener("DOMContentLoaded", () => {
   const deadline = "2024-05-20";
 
   function getTimeRemaining(endTime) {
-    const t = Date.parse(endTime) - Date.parse(new Date()),
-      days = Math.floor(t / (1000 * 60 * 60 * 24)),
-      hours = Math.floor((t / (1000 * 60 * 60)) % 24),
-      minutes = Math.floor((t / (1000 * 60)) % 60),
+    let days, hours, minutes, seconds;
+    const t = Date.parse(endTime) - Date.parse(new Date());
+
+    if (t <= 0) {
+      days = 0;
+      hours = 0;
+      minutes = 0;
+      seconds = 0;
+    } else {
+      days = Math.floor(t / (1000 * 60 * 60 * 24));
+      hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+      minutes = Math.floor((t / (1000 * 60)) % 60);
       seconds = Math.floor((t / 1000) % 60);
+    }
     return {
       total: t,
       days: days,
@@ -59,13 +68,13 @@ window.addEventListener("DOMContentLoaded", () => {
     };
   }
 
-	function getZero(num){
-		if (num >= 0 && num < 10){
-			return `0${num}`
-		} else {
-			return num;
-		}
-	}
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
 
   function setClock(selector, endTime) {
     const timer = document.querySelector(selector),
@@ -73,9 +82,9 @@ window.addEventListener("DOMContentLoaded", () => {
       hours = timer.querySelector("#hours"),
       minutes = timer.querySelector("#minutes"),
       seconds = timer.querySelector("#seconds"),
-			timeInterval = setInterval(updateClock, 1000);
+      timeInterval = setInterval(updateClock, 1000);
 
-			updateClock();
+    updateClock();
 
     function updateClock() {
       const t = getTimeRemaining(endTime);
@@ -85,10 +94,10 @@ window.addEventListener("DOMContentLoaded", () => {
       minutes.innerHTML = getZero(t.minutes);
       seconds.innerHTML = getZero(t.seconds);
 
-			if (t.total <= 0){
-				clearInterval(timeInterval);
-			}
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
     }
   }
-	setClock('.timer', deadline);
+  setClock(".timer", deadline);
 });

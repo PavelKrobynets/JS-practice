1c6d376198a5754c71646335a9c8a9fc839f94ef
Pavel <krobynetspavel@gmail.com>
2024-05-13 13:36:23 +0300

    rest integrated in func

--- src/js/script.js
+++ src/js/script.js
@@ -157,13 +157,14 @@ window.addEventListener("DOMContentLoaded", () => {
   // Using classes for cards
 
   class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
       this.src = src;
       this.alt = alt;
       this.title = title;
       this.descr = descr;
       this.price = price;
-			this.parent = document.querySelector(parentSelector);
+      this.classes = classes;
+      this.parent = document.querySelector(parentSelector);
       this.transfer = 38;
     }
     changeToUAH() {
@@ -172,8 +173,12 @@ window.addEventListener("DOMContentLoaded", () => {
     showOnPage() {
       this.changeToUAH();
       const element = document.createElement("div");
+			if(this.classes[0]){
+				this.classes.forEach((className) => element.classList.add(className));}
+				else{
+					element.classList.add("menu__item");
+				}
       element.innerHTML = `
-					<div class="menu__item">
 					<img src=${this.src} alt=${this.alt}>
 					<h3 class="menu__item-subtitle">${this.title}</h3>
 					<div class="menu__item-descr">${this.descr}</div>
@@ -182,36 +187,37 @@ window.addEventListener("DOMContentLoaded", () => {
 						<div class="menu__item-cost">Цена:</div>
 						<div class="menu__item-total"><span>${this.price}</span> грн/день</div>
 					</div>
-					</div>
 						`;
-						this.parent.append(element);
+      this.parent.append(element);
     }
   }
 
-	new MenuCard(
-		"img/tabs/vegy.jpg",
-		"Fitness",
-		`Меню "Фитнес"`, 
-		`Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!`,
-		9,
-		`.menu .container`
-	).showOnPage();
+  new MenuCard(
+    "img/tabs/vegy.jpg",
+    "Fitness",
+    `Меню "Фитнес"`,
+    `Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!`,
+    9,
+    `.menu .container`
+  ).showOnPage();
 
-	new MenuCard(
-		"img/tabs/elite.jpg",
-		"Premium",
-		`Меню “Премиум”`, 
-		`В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!`,
-		15,
-		`.menu .container`
-	).showOnPage();
+  new MenuCard(
+    "img/tabs/elite.jpg",
+    "Premium",
+    `Меню “Премиум”`,
+    `В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!`,
+    15,
+    `.menu .container`,
+    `menu__item`
+  ).showOnPage();
 
-	new MenuCard(
-		"img/tabs/post.jpg",
-		"Vegan",
-		`Меню "Постное"`, 
-		`Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.`,
-		7,
-		`.menu .container`
-	).showOnPage();
+  new MenuCard(
+    "img/tabs/post.jpg",
+    "Vegan",
+    `Меню "Постное"`,
+    `Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.`,
+    7,
+    `.menu .container`,
+    `menu__item`
+  ).showOnPage();
 });
